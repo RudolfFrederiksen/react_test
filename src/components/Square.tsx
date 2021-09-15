aaa56b3c2a2da30c35fa4aad1ad613ab073e55a9
RudolfFrederiksen <rudolf.frederiksen@gmail.com>
2021-09-15 15:38:42 +0200

    init setup add typescript and scss split base example in multiple classes

---
+++ src/components/Square.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Square.scss";
+
+class Square extends React.Component {
+    render() {
+        return <button className="square">{/* TODO */}</button>;
+    }
+}
+
+export { Square };
